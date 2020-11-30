1851c32624596580f2cf0dd2313decdd966d2c7f
Fr-yayou <yanis.bekhtaoui@gmail.com>
2020-11-30 11:51:07 -0800

    first commit

--- Alumni/routes/api.php
+++ Alumni/routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//API FOR FORUM POST//
+Route::get('/posts','App\Http\Controllers\PostController@index');
+Route::get('/post/{id}','App\Http\Controllers\PostController@show');
+Route::post('/posts/post','App\Http\Controllers\PostController@store');
+Route::put('/post/{id}/edit','App\Http\Controllers\PostController@update');
+Route::delete('/post/{id}','App\Http\Controllers\PostController@destroy');
+
+//API FOR FORUM COMMENT//
+Route::get('/comments','App\Http\Controllers\CommentController@index');
+Route::get('/comment/{id}','App\Http\Controllers\CommentController@show');
+Route::post('/comments/post','App\Http\Controllers\CommentController@store');
+Route::put('/comment/{id}/edit','App\Http\Controllers\CommentController@update');
+Route::delete('/comment/{id}','App\Http\Controllers\CommentController@destroy');
