1b211c19c6d32559d914d487956dacbdb515e6b6
Quentin GUYOT <quentin.guyot@epitech.eu>
2020-12-01 08:23:42 +0100

    JWT token ok and display, delete user

--- Alumni/routes/api.php
+++ Alumni/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+//auth route to login, register...
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('auth/logout', [AuthController::class, 'logout']);
+Route::get('auth/user', [AuthController::class, 'user']);
+Route::post('auth/refresh', [AuthController::class, 'refresh']);
+
+//protected routes - needs to be authenticated
+Route::middleware('auth')->get('user/{id}', [UserController::class, 'show']);
+Route::middleware('auth')->get('users', [UserController::class, 'index']);
+Route::middleware('auth')->delete('user/{id}', [UserController::class, 'destroy']);
+ 
