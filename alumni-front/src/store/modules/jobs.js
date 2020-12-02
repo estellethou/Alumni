1d0c4b61231b0234f5eb4caee2842b829e34b3b8
YoannP-08 <yoann.pons08@gmail.com>
2020-12-02 11:50:14 +0100

    [ADD] updateJob backend logic in store jobs - functionality not tested in front

--- alumni-front/src/store/modules/jobs.js
+++ alumni-front/src/store/modules/jobs.js
@@ -7,20 +7,28 @@ const getters = {
 };
 
 const actions = {
-    async fetchAllJobs({ commit }) {
+    async fetchAllJobs({ commit },) {
         const response = await axios.get(
             'http://localhost:8899/C-DEV-130-PAR-1-1-ecp-estelle.thou/Alumni/public/api/jobs'
         );
-
-        console.log(response.data)
-
         commit('setJobs', response.data)
+    },
+
+    async updateJob ({ commit }, updJob) {
+        console.log(updJob);
+        const response = await axios.put(
+            `http://localhost:8899/C-DEV-130-PAR-1-1-ecp-estelle.thou/Alumni/public/api/job/${updJob.id}/edit`,
+            updJob
+        );
+        console.log(response.data)
+        commit('editJob', response.data)
     },
 
 };
 
 const mutations = {
     setJobs: (state, jobs) => (state.jobs = jobs),
+    editJob: () => { return true },
 
 }; 
 
