8ebab71ac1d919cb40d5385660710a2f4c79e598
YoannP-08 <yoann.pons08@gmail.com>
2020-12-04 20:39:40 +0100

    [MOD] rm unnecessary code

--- alumni-front/src/components/NavBar.vue
+++ alumni-front/src/components/NavBar.vue
@@ -1,17 +1,4 @@
 <template>
-
-    <div class="container-navBar">
-        <div class="container-logo">
-            <img src="" alt="">
-        </div>
-        <div class="container-link">
-            <router-link to="/">Home</router-link>
-            <router-link to="/profile">Profile</router-link>
-            <router-link to="/job">Jobs</router-link>
-            <router-link to="/forum">Forum</router-link>
-        </div>
-    </div>
-
     <ul>
         <li>
             <router-link
