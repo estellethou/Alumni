76053684a0d9c8c6e9b5ca217b841c0a0e22feb2
YoannP-08 <yoann.pons08@gmail.com>
2020-12-08 22:39:50 +0100

    [ADD] a page to display job details when clicking on job add in listjobs

---
+++ alumni-front/src/views/SingleJob.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <v-container v-for="(detail, index) in filterJob" :key="index">
+            <v-card>
+                <v-card-title>
+                    {{ detail.title }}
+                </v-card-title>
+            </v-card>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+    name: "SingleJob",
+
+    methods: {
+        ...mapActions(["fetchAllJobs"]),
+    },
+
+    computed: {
+        ...mapGetters(["getAllJobs"]),
+
+        filterJob() {
+            return this.getAllJobs.filter(job => {
+                return job.id == this.$route.params.id;
+            })
+        },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
