f713a93cb41746d75242b148a1435f6adedb8070
Fr-yayou <yanis.bekhtaoui@gmail.com>
2020-12-09 01:54:45 -0800

    Pagination added in the ListPost component

--- alumni-front/src/components/forumComponents/ListPosts.vue
+++ alumni-front/src/components/forumComponents/ListPosts.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="container-posts">
-        <div v-for="(post,index) in Posts" :key="index">
+        <div v-for="(post,index) in paginatedData" :key="index">
             <Posts class="postsCard" v-bind:post="post"/>
+        </div>
+        <div class="containerPagination">
+            <v-btn class="mr-2" @click="previousPage" :disabled="pageNumber==0" color="primary">prev</v-btn>
+            <v-btn class="ml-2" @click="nextPage" :disabled="pageNumber >= pagecount -1" color="primary">next</v-btn>
         </div>
     </div>
 </template>
@@ -11,6 +15,38 @@ import Posts from "./Posts"
 export default {
     name:"ListPosts",
     props:["Posts"],
+    
+    data(){
+        return{
+            pageNumber:0,
+            size:5
+
+        }
+    },
+
+    methods:{
+        
+        previousPage(){
+            return this.pageNumber --
+        },
+        nextPage(){
+            return this.pageNumber ++
+        },
+
+        pageCount(){
+            let l = this.List.length,
+            s = this.size
+            return Math.ceil(l/s)
+      }
+    },
+
+    computed:{
+        paginatedData(){
+        const start = this.pageNumber * this.size,
+        end = start +this.size
+        return this.Posts.slice(start,end)
+      },
+    },
 
     components:{
         Posts
@@ -25,5 +61,9 @@ export default {
 }
 .postsCard{
     margin-bottom:30px
+}
+.containerPagination{
+    display: flex;
+    justify-content: center;
 }
 </style>
