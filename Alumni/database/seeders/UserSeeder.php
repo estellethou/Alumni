0d57ee45062f25b3417d57a91eac7092b430451f
Bigo <jbsilvestre@hotmail.fr>
2020-12-09 18:23:57 +0100

    seeder for admin

---
+++ Alumni/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'firstname' => 'admin',
+            'lastname' => 'admin',
+            'email' => 'admin@alumni.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
