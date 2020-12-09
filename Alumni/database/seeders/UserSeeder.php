92c734958604ad1fe6e329555976487fd7ebb02b
Bigo <jbsilvestre@hotmail.fr>
2020-12-09 16:47:58 +0100

    add admin user in seeder

---
+++ Alumni/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+
+use Illuminate\Database\Seeder;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(
+                ['email' => 'admin@codingalumni.com'], [
+                    'name' => 'admin',
+                    'password' => bcrypt('12345678'),
+                    'is_admin' => 1,
+                ]
+            );
+    }
+}
