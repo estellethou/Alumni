a9bfb004bb9a08f914c830026182b3266a0da322
Bigo <jbsilvestre@hotmail.fr>
2020-12-10 15:18:46 +0100

    event cards and few tweeks

---
+++ alumni-front/src/components/EventComponents/EventList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex flex-wrap">
+    <div class="ma-2" v-for="event in getAllEvents" :key="event.id">
+      <event-card :event="event"></event-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import EventCard from "../EventComponents/EventCard.vue";
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  components: {
+    EventCard,
+  },
+  name: "EventList",
+  computed: {
+    ...mapGetters(["getAllEvents"]),
+  },
+  methods: {
+    ...mapActions(["setAllEvents"]),
+  },
+  created() {
+    this.setAllEvents();
+  },
+};
+</script>
+
+<style>
+</style>
