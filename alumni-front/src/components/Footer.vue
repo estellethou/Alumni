ff3ac690513296ac627861c9cc156c0b1da9e0d7
YoannP-08 <yoann.pons08@gmail.com>
2020-12-11 13:43:32 +0100

    [ADD] a footer and about page

---
+++ alumni-front/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link :to="`/about`">
+        <h1>ABOUT</h1>
+        </router-link>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Footer',
+
+}
+</script>
+
+<style>
+
+</style>
